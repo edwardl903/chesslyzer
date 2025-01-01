d39e7c598c54a1462b09061eed5774ec905da962
Edward Lai <edwardlai2003@gmail.com>
2025-01-01 02:15:56 -0500

    fixing deploymenet again

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
 from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS  # Import Flask-CORS
 import subprocess
 import os
 
 app = Flask(__name__)
 
-# Enable CORS for all routes
-CORS(app)
+# Enable CORS for the specific frontend origin
+CORS(app, resources={r"/*": {"origins": "https://chesslyzer.vercel.app"}})
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate_csv():
     data = request.json
-    username = data['username']
+    username = data.get('username')
 
     try:
-        # Call the chesslytics.py script to generate the CSV
         result = subprocess.run(
-            ["python", "chesslytics.py", username],  # Adjust the path if necessary
+            ["python", "chesslyzer.py", username],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
 
-        # Assuming the Python script generates a CSV with the username
         csv_file = f"{username}.csv"
-
         if os.path.exists(csv_file):
-            return send_file(csv_file, as_attachment=True, mimetype='text/csv')  # Optional: set MIME type explicitly
+            return send_file(csv_file, as_attachment=True)
 
         return jsonify({"error": "CSV generation failed"}), 500
 
